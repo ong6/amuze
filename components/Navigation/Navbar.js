66ef46b44014d4c554f4fb988d98e5d96b415072
ong6 <junxiongong2@gmail.com>
2022-01-22 16:12:38 +0800

    added metaamsk check and split wallet from navbar

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,9 +1,5 @@
 import { Box, Container, Flex, Heading, Link, Stack } from "@chakra-ui/react";
 import NextLink from "next/link";
-import { useContext } from "react";
-import { IoWallet } from "react-icons/io5";
-import { MetaContext } from "../../context/MetaContext";
-import { getShortAccountHash, login } from "../../pages/api/util";
 import Logo from "../Logo";
 
 const LinkItem = ({ href, path, _target, children, ...props }) => {
@@ -71,36 +67,8 @@ const Navbar = (props) => {
 						Renting/Listing
 					</LinkItem>
 				</Stack>
-				<ConnectWallet />
 			</Container>
 		</Box>
-	);
-};
-
-const ConnectWallet = () => {
-	const [address, metamask] = useContext(MetaContext);
-
-	return (
-		<div className="fixed top-0 right-0 p-8 z-50">
-			<button
-				onClick={login}
-				className="border border-white text-center rounded-full px-2 py-1 md:px-6 md:py-3 items-center"
-				disabled={metamask}>
-				<div
-					className={
-						address
-							? `text-lg md:text-2xl font-bold p-2 text-orange-500 inline-flex items-center gap-2`
-							: `text-lg md:text-2xl font-bold p-2 text-white inline-flex items-center gap-2`
-					}>
-					<IoWallet className="mt-1" />
-					{metamask
-						? "Install MetaMask!"
-						: address
-						? getShortAccountHash(address)
-						: "Connect wallet"}
-				</div>
-			</button>
-		</div>
 	);
 };
 
