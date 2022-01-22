883ea505397f9969b95c61599c3c28e1db73a241
ong6 <junxiongong2@gmail.com>
2022-01-23 01:10:35 +0800

    added ipfs and some updates

--- components/museum/Entry.js
+++ components/museum/Entry.js
@@ -7,13 +7,143 @@ import {
 	Image,
 	Input,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { Go } from "../../public/museum_pic/go.svg";
 import { FaEthereum } from "react-icons/fa";
 import { BsArrowDownCircle } from "react-icons/bs";
 import { GiTicket } from "react-icons/gi";
+import Coin from "../../public/logo.svg";
+import { ethers } from "ethers";
 
 export default function Entry() {
+	const [muze, setMuze] = useState(null);
+
+	const onSubmit = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		await provider.send("eth_requestAccoun ts", []);
+		const signer = provider.getSigner();
+	};
+
+	const addMuze = async () => {
+		const contractAddr = "0xec91c38f021149f72e4d7788c39cf3d941afa3a6";
+		const tokenSymbol = "MUZE";
+		const tokenDecimals = 18;
+		const tokenImage = window.location.href + Coin.src;
+
+		try {
+			const wasAdded = await ethereum.request({
+				method: "wallet_watchAsset",
+				params: {
+					type: "ERC20", // Initially only supports ERC20, but eventually more!
+					options: {
+						address: contractAddr, // The address that the token is at.
+						symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+						decimals: tokenDecimals, // The number of decimals in the token
+						image: tokenImage, // A string url of the token logo
+					},
+				},
+			});
+
+			if (wasAdded) {
+				console.log("Thanks for adding!");
+			} else {
+				console.log("Your loss!");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	function TopSection() {
+		const [ether, setEther] = useState("");
+		const handleChange = (event) => setEther(event.target.value);
+
+		return (
+			<div className="flex flex-col pt-2 space-y-8 p-3 ">
+				<div className="flex flex-col space-y-2">
+					<div className="flex flex-row justify-between text-gary-900">
+						<div className="div">Pay</div>
+						<div className=" underline">Available:0.05</div>
+					</div>
+					<div className="border-4 rounded-xl border-orange-400 px-4 py-3">
+						<Box
+							display="flex"
+							alignItems="center"
+							justifyContent="space-between">
+							<span className="flex flex-row items-center space-x-4">
+								<Icon as={FaEthereum} w={5} h={5} />
+								<div className="font-semibold text-base">ETH</div>
+							</span>
+							<Input
+								variant="unstyled"
+								placeholder="0.01"
+								w="30%"
+								className="text-right font-bold"
+								onChange={handleChange}
+								value={ether}
+								type="number"
+								size="lg"
+							/>
+						</Box>
+					</div>
+				</div>
+				<Icon as={BsArrowDownCircle} w={8} h={8} className="self-center" />
+				<div className="flex flex-col space-y-2">
+					<div className="flex flex-row justify-between text-gary-900">
+						<div className="div">Receive (Estimated)</div>
+						<div className=" underline">Available:0.00</div>
+					</div>
+					<div className="border-4 rounded-xl border-orange-400 px-4 py-3">
+						<Box
+							display="flex"
+							alignItems="center"
+							justifyContent="space-between">
+							<span className="flex flex-row items-center space-x-4">
+								<Image src={"/favicon.ico"} w={5} h={5} alt="coin" />
+								<div className="font-semibold text-base">MUZE</div>
+							</span>
+							<Input
+								variant="unstyled"
+								placeholder="0.00"
+								w="30%"
+								className="text-right font-bold"
+								value={ether * 3300}
+								size="lg"
+								isReadOnly
+							/>
+						</Box>
+					</div>
+				</div>
+				<div className="self-center font-base text-gray-700 text-sm">
+					1 ETH = 3300 MUZE
+				</div>
+				<div className="flex flex-col space-y-2">
+					<Button
+						className="w-full"
+						colorScheme="orange"
+						size="lg"
+						bg={"orange.400"}
+						rounded={15}
+						onClick={addMuze}>
+						Add $MUZE
+					</Button>
+					<Button
+						className="w-full"
+						colorScheme="orange"
+						size="lg"
+						bg={"orange.400"}
+						rounded={15}
+						onClick={onSubmit}>
+						Confirm Order
+					</Button>
+					<div className="self-center font-base text-gray-700 text-sm">
+						Enter an amount to see more trading details
+					</div>
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<Container className="bg-white p-8 self-center rounded-xl" maxW="lg">
 			<div className="flex flex-col space-y-4">
@@ -23,33 +153,12 @@ export default function Entry() {
 						<Image src="/museum_pic/go.svg" alt="go" layout="fill" />
 						<div className="font-bold text-2xl text-orange-400">SWAP $MUZE</div>
 					</span>
+					<span>Entrance Fee: {30} MUZE</span>
 				</div>
 				{/* payment section */}
-
-				<div className="flex flex-col pt-2 space-y-8 p-3 ">
-					<InputEntry />
-					<Icon as={BsArrowDownCircle} w={8} h={8} className="self-center" />
-					<InputReceive />
-					<div className="self-center font-base text-gray-700 text-sm">
-						1 ETH = 3300 MUZE
-					</div>
-					<div className="flex flex-col space-y-2">
-						<Button
-							className="w-full"
-							colorScheme="orange"
-							size="lg"
-							bg={"orange.400"}
-							rounded={15}>
-							Confirm Order
-						</Button>
-						<div className="self-center font-base text-gray-700 text-sm">
-							Enter an amount to see more trading details
-						</div>
-					</div>
-				</div>
+				<TopSection />
 
 				<Divider borderColor={"purple.500"} />
-
 				{/* enter section */}
 				<div className="flex flex-col pt-4 px-4 space-y-6">
 					<div className="flex flex-row items-center justify-center">
@@ -69,79 +178,5 @@ export default function Entry() {
 				</div>
 			</div>
 		</Container>
-	);
-}
-
-function InputEntry() {
-	const [value, setValue] = React.useState("");
-	const handleChange = (event) => setValue(event.target.value);
-
-	return (
-		<div>
-			<div className="flex flex-col space-y-2">
-				<div className="flex flex-row justify-between text-gary-900">
-					<div className="div">Pay</div>
-					<div className=" underline">Available:0.05</div>
-				</div>
-				<div className="border-4 rounded-xl border-orange-400 px-4 py-3">
-					<Box
-						display="flex"
-						alignItems="center"
-						justifyContent="space-between">
-						<span className="flex flex-row items-center space-x-4">
-							<Icon as={FaEthereum} w={5} h={5} />
-							<div className="font-semibold text-base">ETH</div>
-						</span>
-						<Input
-							variant="unstyled"
-							placeholder="0.01"
-							w="30%"
-							className="text-right font-bold"
-							onChange={handleChange}
-							value={value}
-							type="number"
-							size="lg"
-						/>
-					</Box>
-				</div>
-			</div>
-		</div>
-	);
-}
-
-function InputReceive() {
-	const [value, setValue] = React.useState("");
-	const handleChange = (event) => setValue(event.target.value);
-
-	return (
-		<div>
-			<div className="flex flex-col space-y-2">
-				<div className="flex flex-row justify-between text-gary-900">
-					<div className="div">Receive (Estimated)</div>
-					<div className=" underline">Available:0.00</div>
-				</div>
-				<div className="border-4 rounded-xl border-orange-400 px-4 py-3">
-					<Box
-						display="flex"
-						alignItems="center"
-						justifyContent="space-between">
-						<span className="flex flex-row items-center space-x-4">
-							<Image src={"/favicon.ico"} w={5} h={5} alt="coin" />
-							<div className="font-semibold text-base">MUZE</div>
-						</span>
-						<Input
-							variant="unstyled"
-							placeholder="0.00"
-							w="30%"
-							className="text-right font-bold"
-							onChange={handleChange}
-							value={value}
-							type="number"
-							size="lg"
-						/>
-					</Box>
-				</div>
-			</div>
-		</div>
 	);
 }
--- pages/museum/singapore.js
+++ pages/museum/singapore.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { useContext } from "react";
 import Layout from "../../components/layouts/Museum";
 import Entry from "../../components/museum/Entry";
 import Section from "../../components/Section";
+import { MetaContext } from "../../context/MetaContext";
 
 export default function Museum() {
+	const { address } = useContext(MetaContext);
+
 	return (
 		<Layout>
 			<Head>
@@ -11,11 +15,17 @@ export default function Museum() {
 				<meta name="description" content="Amuze-Museum at your fingertips" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Section delay={0.2}>
-				<div className="p-8">
-					<Entry />
+			{address ? (
+				<Section delay={0.2}>
+					<div className="p-8">
+						<Entry />
+					</div>
+				</Section>
+			) : (
+				<div className="[height:50vh] flex text-4xl text-white items-center justify-center">
+					Please Connect Your wallet to view Renting and Listing.
 				</div>
-			</Section>
+			)}
 		</Layout>
 	);
 }
