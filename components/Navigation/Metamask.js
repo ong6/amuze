414d2face4c9261fc232bfb209608d7c9856c2cc
ong6 <junxiongong2@gmail.com>
2022-01-26 10:12:41 +0800

    Darken text on wallet

--- components/Navigation/Metamask.js
+++ components/Navigation/Metamask.js
@@ -17,13 +17,13 @@ const ConnectWallet = ({ address, setAddress }) => {
     <div className="fixed top-0 right-0 p-8 z-50">
       <button
         onClick={login}
-        className="border border-white text-center rounded-full px-2 py-1 md:px-6 md:py-3 items-center bg-gray-500 transition-all duration-200 hover:bg-purple-600"
+        className="border border-white text-center rounded-full px-2 py-1 md:px-6 md:py-3 items-center bg-gray-200 transition-all duration-200 hover:bg-purple-600"
       >
         <div
           className={
             address
               ? `text-lg xl:text-2xl font-bold xl:p-2 text-orange-500 inline-flex items-center gap-2`
-              : `text-lg xl:text-2xl font-bold xl:p-2 text-white inline-flex items-center gap-2`
+              : `text-lg xl:text-2xl font-bold xl:p-2 text-gray-600 inline-flex items-center gap-2`
           }
         >
           <IoWallet className="" />
