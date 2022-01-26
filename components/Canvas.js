401ab22e774d24fe33bfd132c1609e4a06aee973
ong6 <junxiongong2@gmail.com>
2022-01-26 15:36:55 +0800

    fixed souvenir

--- components/Canvas.js
+++ components/Canvas.js
@@ -4,43 +4,41 @@ import { changeratio } from "../pages/api/util.js";
 import img from "../public/souvenir_template.png";
 
 const Canvas = (props) => {
-	const canvasRef = useRef(null);
-	const imgData = props.imgdata;
-	const oldTxt = "";
-	// const txt = document.getElementById('msg').value;
+  const canvasRef = useRef(null);
+  const imgData = props.imgdata;
 
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext("2d");
-		canvas.width = changeratio(2510);
-		canvas.height = changeratio(1930);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = changeratio(2510);
+    canvas.height = changeratio(1930);
 
-		let imgObj = new Image();
-		let imageSrc = img.src;
+    let imgObj = new Image();
+    let imageSrc = img.src;
 
-		function addImageProcess(src) {
-			return new Promise(() => {
-				imgObj.crossOrigin = "anonymous";
-				imgObj.onload = () => {
-					let w = canvas.width;
-					let nw = imgObj.naturalWidth;
-					let nh = imgObj.naturalHeight;
-					let aspect = nw / nh;
-					let h = w / aspect;
-					canvas.height = h;
-					context.drawImage(imgObj, 0, 0, w, h);
+    function addImageProcess(src) {
+      return new Promise(() => {
+        imgObj.crossOrigin = "anonymous";
+        imgObj.onload = () => {
+          let w = canvas.width;
+          let nw = imgObj.naturalWidth;
+          let nh = imgObj.naturalHeight;
+          let aspect = nw / nh;
+          let h = w / aspect;
+          canvas.height = h;
+          context.drawImage(imgObj, 0, 0, w, h);
 
-					drawText(context);
-					// document.getElementById('msg').addEventListener('input', drawText(context));
-				};
-				imgObj.src = src;
-			});
-		}
+          drawText(context);
+          // document.getElementById('msg').addEventListener('input', drawText(context));
+        };
+        imgObj.src = src;
+      });
+    }
 
-		addImageProcess(imageSrc);
-	}, [imgData]);
+    addImageProcess(imageSrc);
+  }, [imgData]);
 
-	return <canvas ref={canvasRef} {...props} />;
+  return <canvas ref={canvasRef} {...props} />;
 };
 
 export default Canvas;
--- pages/museum/singapore/souvenir.js
+++ pages/museum/singapore/souvenir.js
@@ -1,56 +1,127 @@
 import Head from "next/head";
 import Image from "next/image";
-import Canvas from "../../../components/Canvas"
+import Canvas from "../../../components/Canvas";
 import { useRouter } from "next/router";
 import Layout from "../../../components/layouts/Default";
 import Section from "../../../components/Section";
-import { Text } from "@chakra-ui/react";
+import {
+  Text,
+  FormLabel,
+  Input,
+  Heading,
+  FormControl,
+  Container,
+  Button,
+  Textarea,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+  Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function Museum() {
-//   const testData = {
-//     noOfTrades: 456,
-//     gasUSD: 2000,
-//     value: 10000,
-//     favPair: "DOGE / ETH",
-//     favPairValue: 150840,
-//     avgTrade: 5940,
-//     maxTrade: 54120,
-//     maxDate: 1641212754,
-//     minTrade: 520,
-//     minDate: 1641212754,
-//   };
+export default function Souvenir() {
+  const [imgData, setImgData] = useState({
+    name: "temp",
+    description: "temp",
+  });
+
+  const styles = {
+    heading: "text-left text-2xl font-semibold text-gray-600",
+    headers: "text-left text-sm text-gray-600 uppercase",
+    select: "bg-gray-100",
+  };
+
+  const {
+    isOpen: isPostcardOpen,
+    onOpen: onPostcardOpen,
+    onClose: onPostcardClose,
+  } = useDisclosure();
+
   return (
     <Layout>
-      <div>
-        <Head>
-          <title>Amuze</title>
-          <meta name="description" content="Amuze-Museum at your fingertips" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Section delay={0.2}>
-          <div className="items-center text-center justify-center ">
-            <div
-              className="font-bold text-black uppercase whitespace-pre text-center"
-              id="claimSuccess"
-            >
-              <Text color="white">View your Souvenir!</Text>
+      <Section delay={0.2}>
+        <div className="items-center text-center justify-center space-y-8 pb-40">
+          <Heading color="white">View your Souvenir!</Heading>
+          <Container className="bg-white rounded-xl">
+            <div className="flex flex-col space-y-4 p-4">
+              <div className="div">
+                <div className={styles.heading}>Generate Postcard</div>
+                <div className="text-left font-base text-gray-500 text-sm">
+                  Please fill in the following to get a postcard that you can
+                  keep!
+                </div>
+              </div>
+              <FormControl className="space-y-6">
+                <FormLabel
+                  htmlFor="name"
+                  className={"text-left text-sm text-gray-600 uppercase"}
+                >
+                  Visitor Name:
+                </FormLabel>
+                <Input
+                  id="name"
+                  placeholder="name"
+                  variant="filled"
+                  size={"sm"}
+                  value={imgData.name}
+                  onChange={(e) =>
+                    setImgData({
+                      ...imgData,
+                      name: e.target.value,
+                    })
+                  }
+                ></Input>
+                <FormLabel
+                  htmlFor="description"
+                  className={"text-left text-sm text-gray-600 uppercase"}
+                >
+                  How was your experience
+                </FormLabel>
+                <Textarea
+                  id="description"
+                  placeholder="description"
+                  variant="filled"
+                  size={"sm"}
+                  value={imgData.description}
+                  onChange={(e) =>
+                    setImgData({
+                      ...imgData,
+                      description: e.target.value,
+                    })
+                  }
+                ></Textarea>
+              </FormControl>
+              <Button colorScheme="blue" onClick={onPostcardOpen}>
+                Generate postcard
+              </Button>
             </div>
+          </Container>
+        </div>
+      </Section>
+
+      <Modal isOpen={isPostcardOpen} onClose={onPostcardClose}>
+        <ModalOverlay />
+        <ModalContent
+          minW={"90%"}
+          justifyItems="center"
+          className="text-center"
+          p={6}
+          bg="gray.100"
+        >
+          <Box className="space-y-4">
             <Canvas
-              id="roadMapDrawing"
-              className="flex mx-auto h-[70vh]"
-            //   imgdata={testData}
+              id="postcard"
+              className="flex mx-auto w-[80vw] rounded-lg"
+              imgData={imgData}
             />
-            <br/>
-            <p>
-              <input
-                type="text"
-                id="msg"
-                placeholder="write your message here"
-              />
-            </p>
-          </div>
-        </Section>
-      </div>
+            <Button colorScheme="blue" w="50%">
+              Download
+            </Button>
+          </Box>
+        </ModalContent>
+      </Modal>
     </Layout>
   );
 }
