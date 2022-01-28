5eefbb04dca1627942fbda852d90bc3af3891757
ong6 <junxiongong2@gmail.com>
2022-01-29 00:02:15 +0800

    update wallet/ abi

--- pages/api/wallet.js
+++ pages/api/wallet.js
@@ -1,8 +1,8 @@
 import { ethers } from "ethers";
 
 const tourAddress = "0x57B6A575aa2574A561C26A1297659803e6328aEa";
-const custodyAddress = "0x673aD415952a04BfF71708196D0972B58f1d1181";
-const muzeAddress = "0x2D453dD1c6795f7Af687ddFD228df83153146483";
+const custodyAddress = "0x0Ee09710D1ca4E6AE555aEafc3D12A76F0fCD66B";
+const muzeAddress = "0xfc4931C661963A3ABF4f9BE6c5902b3E12f9bBb9";
 const whiteListedHash =
   "0x8429d542926e6695b59ac6fbdcd9b37e8b1aeb757afab06ab60b1bb5878c3b49";
 
@@ -67,11 +67,11 @@ export const handlePayment = async (address) => {
 
 export const swapEthToMuze = async (ether) => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const abi = ["function fakeSwap() external payable"];
+  const abi = ["function swap() external payable"];
   const signer = provider.getSigner();
   const muzeErc20 = new ethers.Contract(muzeAddress, abi, signer);
 
-  await muzeErc20.fakeSwap({
+  await muzeErc20.swap({
     value: ethers.utils.parseUnits(ether, "ether").toString(),
   });
 };
